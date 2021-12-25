02b177a9f23d6de26ca18a358d575a59717ab42d
Neeraj Srivastava <neeraj.srivastava@ibm.com>
2021-12-25 12:36:58 +0530

    added time zone

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 import './Header.css'
-import { IoIosMail, IoMdCall} from "react-icons/io";
-import {AiOutlineHome} from "react-icons/ai"
+import { IoIosMail, IoMdCall } from "react-icons/io";
+import { AiOutlineHome } from "react-icons/ai";
+import {MdDateRange} from "react-icons/md"
+import Clock from 'react-live-clock';
 
 class Header extends Component {
     render() {
         return (
             <div className='header row'>
+                <div className='col' style={{ color: "white",paddingTop:"3%"}}>
+                    <MdDateRange/> &nbsp;
+                    <Clock 
+                        format={'dddd, DD MMMM YYYY, h:mm:ss A'}
+                        timezone={'GMT/UTC '} ticking={true} />
+                </div>
                 <div className='header-text col'>
                     Hello Visitors !
                 </div>
-                <div className='col' style={{ color: "white",paddingTop:"15px" }}>
-                    <AiOutlineHome/>&nbsp; New Ashok Nagar, New Delhi<br/>
-                    <IoIosMail /> &nbsp; nrjsrivastav100@yahoo.com  &nbsp;<br/>
-                    <IoMdCall /> &nbsp; +91- 8284955191 
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <div className='col' style={{ color: "white", paddingTop: "15px" }}>
+                    <AiOutlineHome />&nbsp; New Ashok Nagar, New Delhi<br />
+                    <IoIosMail /> &nbsp; nrjsrivastav100@yahoo.com  <br />
+                    <IoMdCall /> &nbsp; +91- 8284955191
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
                 </div>
 
